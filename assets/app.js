3e475040952e7dfb11cf2552a926f5731114a150
Wratten <daniel.wratten@gmail.com>
2022-01-19 15:06:03 +0800

    added html structure and pseudocode, linked apis

---
+++ assets/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  // OpenWeather API Key
+  const apiKey = "ece65a809c550038c7bb3d98710f156b";
+
+  // Define selectors for weather information elemeents
+
+  // define selectors for form elements
+
+  // make array for past searched cities
+
+  // use local storage to save searched cities
+
+  // load cities
+
+  // save cities
+
+  // build the url using city input for the api
+
+  // make function to display recently searched cities
+
+  // search for weather conditions
+  // retrieve weather data
+  // store current city in past cities
+  // if there is two of the same city, remove it
+  // update dom elements with current weather
+  // get uv index and 5 day forecast
+  // display 5 day forecast in dom
+
+  // display last city searched on page load
+
+  // handle search button
+  // prevent default so page doesnt reload
+
+  // get rid of the city input when search
+
+  // build the query url with the input
+
+  // make the recently searched cities into buttons that load resepective cities weather
+
+  // when page loads
+  // load cities
+  // display weather for last searched city
+});
