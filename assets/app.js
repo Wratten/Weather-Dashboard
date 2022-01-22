f50527d04438d4fa51c18d1f046a129299e25f3d
Wratten <daniel.wratten@gmail.com>
2022-01-22 20:38:01 +0800

    added 5 day forecast

--- assets/app.js
+++ assets/app.js
@@ -5,17 +5,26 @@ $(document).ready(function () {
   const searchForm = document.getElementById("form-search");
   const cityInput = document.getElementById("city-input");
 
+  const dateTodayElement = document.getElementById("date-today");
+  const chosenCitySpan = document.getElementById("chosen-city-span");
   const currentTempSpan = document.getElementById("current-temp-span");
   const currentHumiditySpan = document.getElementById("current-humidity-span");
   const currentWindSpan = document.getElementById("current-wind-span");
   const currentUvSpan = document.getElementById("current-uv-span");
-  const dateTodayElement = document.getElementById("date-today");
-  const chosenCitySpan = document.getElementById("chosen-city-span");
-
   const weatherCards = document.getElementById("weather-cards");
-  const fiveDayTemperature = document.querySelector(".five-day-temperature");
-  const fiveDayWind = document.querySelector(".five-day-wind");
-  const fiveDayHumidity = document.querySelector(".five-day-wind");
+
+  let searchedCities = [];
+
+  //   function storeCities() {
+  //     localStorage.setItem("serchedCities", JSON.stringify(searchedCities));
+  //   }
+
+  //   function loadCities() {
+  //     const storedCities = JSON.parse(localStorage.getItem("searchedCities"));
+  //     if (storedCities !== "") {
+  //       searchedCities = storedCities;
+  //     }
+  //   }
 
   $.getJSON(
     "https://api.openweathermap.org/data/2.5/weather?q=Perth&appid=ece65a809c550038c7bb3d98710f156b",
@@ -54,12 +63,18 @@ $(document).ready(function () {
       .then(function (data) {
         // update the weather details on page using the data
         chosenCitySpan.textContent = "Current Weather in " + data.name;
-        // NEED TO PUT IN TODAYS DATE
+        // TODAYS DATE
+        dateTodayElement.textContent = moment
+          .unix(data.dt)
+          .format("MMMM Do, YYYY");
+        // TEMPERATURE
         currentTempSpan.textContent =
           kelvinToCelcius(data.main.temp).toFixed(2) + "°C";
+        // HUMIDITY
         currentHumiditySpan.textContent = data.main.humidity + "%";
-        currentWindSpan.textContent = data.wind.speed + " Km/ph";
-        // PUT IN UV INDEX USING OTHER API
+        // WIND
+        currentWindSpan.textContent = data.wind.speed + " m/h";
+        // UV INDEX
         currentUvSpan.textContent = data;
 
         // call the one api to get coordinates for the uv index
@@ -88,17 +103,80 @@ $(document).ready(function () {
           currentUvSpan.setAttribute("class", "extreme");
         }
 
-        const getForecast = function (data) {
-          if (index > 0 && index < 6) {
-            data.daily.forEach(function (value, index) {
-              fiveDayTemperature.textContent =
-                kelvinToCelcius(data.main.temp).toFixed(2) + "°C";
-              fiveDayHumidity.textContent = data.main.humidity + "%";
-              fiveDayWind.textContent = data.wind.speed + " Km/ph";
-            });
-          }
-        };
-        getForecast();
+        const next5Days = oneCallData.daily.slice(0, 6);
+        // reset the data so we dont make more than 5 days
+        weatherCards.textContent = "";
+        // start the loop at 1 to ignore the current day already being displayed
+        for (let i = 0; i < next5Days.length; i++) {
+          const forecast = next5Days[i];
+          // use moment to convert unix into human time
+
+          const col = createWeatherCol(
+            forecast.dt,
+            "",
+            forecast.temp.day,
+            forecast.humidity,
+            forecast.wind_speed,
+            forecast.weather[0].icon
+          );
+          weatherCards.appendChild(col);
+        }
       });
   });
+
+  function createWeatherCol(date, icon, temp, humidity, wind) {
+    const col = document.createElement("div");
+    col.setAttribute("class", "col-2 text-center col-5");
+
+    const card = document.createElement("div");
+    card.setAttribute("class", "card ");
+    col.appendChild(card);
+
+    const cardBody = document.createElement("div");
+    cardBody.setAttribute("class", "card-body p-1 ");
+    card.appendChild(cardBody);
+
+    const dateHeading = document.createElement("p");
+    dateHeading.setAttribute("class", "card-title");
+    cardBody.appendChild(dateHeading);
+
+    //format unix date to days
+    dateHeading.textContent = moment.unix(date).add(1, "d").format("dddd");
+
+    //moment.unix(date).format("dddd");
+
+    // input icon
+    const iconEl = document.createElement("img");
+    iconEl.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/` + icon + `.png`
+    );
+    cardBody.appendChild(iconEl);
+
+    const p = document.createElement("p");
+    cardBody.appendChild(p);
+    const ul = document.createElement("ul");
+    cardBody.appendChild(ul);
+
+    //TEMPERATURE ELEMENT
+    const tempElement = document.createElement("p");
+    tempElement.setAttribute("class", "card-title text-muted");
+    tempElement.textContent = kelvinToCelcius(temp).toFixed(2) + "°C";
+    ul.appendChild(tempElement);
+
+    //WIND ELEMENT
+    const windElement = document.createElement("p");
+    windElement.setAttribute("class", "card-title text-muted");
+    windElement.textContent = "wind: " + wind + " m/s"; //wind_speed;
+    ul.appendChild(windElement);
+
+    ///HUMIDITY ELEMENT
+    const humidityElement = document.createElement("p");
+    humidityElement.setAttribute("class", "card-title text-muted");
+    humidityElement.textContent = humidity + "% humidity";
+    ul.appendChild(humidityElement);
+    p.appendChild(ul);
+
+    return col;
+  }
 });
